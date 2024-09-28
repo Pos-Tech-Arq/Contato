ad1e5579261b45d05bc4978187f299ee6152a26c
Natan Barros <natanbarros133@gmail.com.br>
2024-09-28 16:38:07 -0300

    add constructor

--- src/Contato.Infra/Configurations/ConfigureOpenTelemetryExtension.cs
+++ src/Contato.Infra/Configurations/ConfigureOpenTelemetryExtension.cs
@@ -20,7 +20,10 @@ public static class ConfigureOpenTelemetryExtension
                 metrics.AddView("request-duration",
                     new ExplicitBucketHistogramConfiguration
                     {
-                        Boundaries = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.7, 1, 2.5, 5, 10],
+                        Boundaries = new[]
+                        {
+                            0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.7, 1, 2.5, 5, 10
+                        },
                     });
             });
     }
